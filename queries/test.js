9b7a7d48aa85f8bd72834692661fb3014f8de94d
letung2302 <letritung2302@gmail.com>
2025-12-17 20:24:20 +0700

    Initial commit

---
+++ queries/test.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+
+
+
+db.test.insertMany([
+  {
+    name: "Test Document",
+    value: 42,
+    describe: "tung yeu",
+    created_at: new Date()
+  }
+]);
+
+print("=== SELECT RESULT ===");
+
+db.test.find({ describe: "tung yeu" }).pretty();
